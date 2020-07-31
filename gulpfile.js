e01189488c6e453fee0ed5c981e375afb4bd3ba2
him <yopinwala@gmail.com>
2020-07-31 12:07:08 +0800

    Use gulp for grab gov data instead

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,34 @@
 var gulp = require('gulp');
 var concat = require('gulp-concat');
+var download = require('gulp-download');
+var rename = require('gulp-rename');
+var JSONtoJS = require('gulp-json-to-js');
+
+/*----------------------------------------------
+ * Update cases
+ ----------------------------------------------*/
+gulp.task('update_cases_chi', () => {
+  return download(getDataUrl('chi'))
+  .pipe(rename('gov_data_chi.json'))
+  .pipe(JSONtoJS())
+  .pipe(gulp.dest('./dist/scripts/'));
+});
+
+gulp.task('update_cases_eng', () => {
+  return download(getDataUrl('eng'))
+  .pipe(rename('gov_data_eng.json'))
+  .pipe(JSONtoJS())
+  .pipe(gulp.dest('./dist/scripts/'));
+});
+
+gulp.task('update_cases', gulp.series(
+  'update_cases_chi', 
+  'update_cases_eng'
+));
+
+function getDataUrl(locale) {
+  return `https://api.data.gov.hk/v2/filter?q=%7B%22resource%22%3A%22http%3A%2F%2Fwww.chp.gov.hk%2Ffiles%2Fmisc%2Fbuilding_list_${locale}.csv%22%2C%22section%22%3A1%2C%22format%22%3A%22json%22%7D`;
+}
 
 /*----------------------------------------------
  * Libraries
@@ -25,13 +54,9 @@ gulp.task('lib_css', () => {
 		.pipe(gulp.dest('./dist/styles/'))
 });
 
-// Get gov data
-gulp.task('grab_data', () => {
-  return 
-});
-
 // Task chain
 gulp.task('default', gulp.series(
+  'update_cases',
   'lib_js', 
   'lib_css'
 ));
