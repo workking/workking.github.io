4d2be6a740ac64ae015748cbb99658ab1b7f421b
him <yopinwala@gmail.com>
2020-07-31 19:22:04 +0800

    Clean up

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,7 @@
 var gulp = require('gulp');
-var concat = require('gulp-concat');
 var download = require('gulp-download');
 var rename = require('gulp-rename');
 var JSONtoJS = require('gulp-json-to-js');
-
 
 function getDataUrl(locale) {
   return `https://api.data.gov.hk/v2/filter?q=%7B%22resource%22%3A%22http%3A%2F%2Fwww.chp.gov.hk%2Ffiles%2Fmisc%2Fbuilding_list_${locale}.csv%22%2C%22section%22%3A1%2C%22format%22%3A%22json%22%7D`;
