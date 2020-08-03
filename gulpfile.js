edfb072dff8d9002043591f5d93f42d6462a5eb8
him <yopinwala@gmail.com>
2020-08-03 16:01:51 +0800

    Update list, add terms and conditions

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,8 @@ var gulp = require('gulp');
 var download = require('gulp-download');
 var rename = require('gulp-rename');
 var JSONtoJS = require('gulp-json-to-js');
+var htmlreplace = require('gulp-html-replace');
+var moment = require('moment');
 
 function getDataUrl(locale) {
   return `https://api.data.gov.hk/v2/filter?q=%7B%22resource%22%3A%22http%3A%2F%2Fwww.chp.gov.hk%2Ffiles%2Fmisc%2Fbuilding_list_${locale}.csv%22%2C%22section%22%3A1%2C%22format%22%3A%22json%22%7D`;
@@ -21,9 +23,16 @@ gulp.task('update_cases_eng', () => {
   .pipe(gulp.dest('./'));
 });
 
+gulp.task('update_html', function() {
+  return gulp.src('index.html')
+  .pipe(htmlreplace({ last_update: `<!-- build:last_update -->${moment().format('YYYY-MM-DD')}<!-- endbuild -->` }))
+  .pipe(gulp.dest('./'));
+});
+
 gulp.task('update_cases', gulp.series(
   'update_cases_chi', 
-  'update_cases_eng'
+  'update_cases_eng',
+  'update_html'
 ));
 
 // Task chain
